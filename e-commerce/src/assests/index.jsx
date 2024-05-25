06d203740642890111653957a7ae96ab7c81c5db
hassanupmyranks <hassanmufez@futuretechschool.in>
2024-05-25 10:01:00 +0530

    e-commerce-3

---
+++ e-commerce/src/assests/index.jsx
@@ -0,0 +1,14 @@
+import banner1 from './banner-1.jpg';
+import banner2 from './banner-2.png';
+import banner3 from './banner-3.jpg';
+import banner4 from './banner-4.jpg';
+import banner5 from './banner-5.jpg';
+
+export {
+        banner1,
+        banner2,
+        banner3,
+        banner4,
+        banner5,
+    
+};
--- e-commerce/src/customer/component/HomeCarosel/MainCarosel.jsx
+++ e-commerce/src/customer/component/HomeCarosel/MainCarosel.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import { banner1 } from '../../../assests';
 
 const responsive = {
-    0: { items: 1 },
+    0: { items: banner1 },
     568: { items: 2 },
     1024: { items: 3 },
 };
